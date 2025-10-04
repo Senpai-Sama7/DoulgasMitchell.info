6e8bf69aee6c3f4f7f0e0d2c8f9700e8894bc291
Douglas Mitchell <senpai-sama7@proton.me>
2025-10-04 08:09:29 -0500

    Update DoulgasMitchell.info/frontend/pages/_document.js
    Co-authored-by: qodo-merge-pro[bot] <151058649+qodo-merge-pro[bot]@users.noreply.github.com>

--- DoulgasMitchell.info/frontend/pages/_document.js
+++ DoulgasMitchell.info/frontend/pages/_document.js
@@ -4,13 +4,7 @@ export default function Document(){
   return (
     <Html lang="en">
       <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
-          integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
+        {/* The <link> tag for Font Awesome should be removed from here and imported in _app.js */}
       </Head>
       <body>
         <Main />
